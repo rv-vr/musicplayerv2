554c55ab31d86420e09e256e1d8497a62ad1e80c
Vince <qsxdrtgbnmjuy@protonmail.ch>
2026-07-22 15:05:51 +0800

    build: copy libbassflac.so to dist

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ mkdir -p dist/lib
 cp build/musicplayer dist/
 cp lib/libbass.so dist/lib/
 cp lib/libbass_aac.so dist/lib/
+cp lib/libbassflac.so dist/lib/
 cp style.qss dist/
 cp run_import.sh dist/
 cp clean_flac.py dist/
