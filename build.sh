67a3992844a5bb0311fc0ba9913fa342a4142edf
Vince <qsxdrtgbnmjuy@protonmail.ch>
2026-07-22 15:20:11 +0800

    build: remove python scripts from dist package

--- build.sh
+++ build.sh
@@ -17,10 +17,6 @@ cp lib/libbass.so dist/lib/
 cp lib/libbass_aac.so dist/lib/
 cp lib/libbassflac.so dist/lib/
 cp style.qss dist/
-cp run_import.sh dist/
-cp clean_flac.py dist/
-cp lrcput.py dist/
-cp extract_metadata.py dist/
 
 echo "=== Done ==="
 echo "Run: cd dist && ./musicplayer"
